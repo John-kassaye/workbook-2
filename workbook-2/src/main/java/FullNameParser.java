72c1536bdfaf0b479f795dbb0a453b48a73a06f1
Yohannes Kassaye <jmule1567@gmail.com>
2025-04-16 19:49:27 -0700

    initial commit

--- workbook-2/src/main/java/FullNameParser.java
+++ workbook-2/src/main/java/FullNameParser.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FullNameParser {
+    static Scanner fullnames = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        setFullName();
+    }
+    public static void setFullName(){
+        System.out.println("Please enter your full name");
+        String name = fullnames.nextLine();
+        String[] name2 = name.split(" ");
+
+        String firstName = name2[0];
+        String secondName = "none";
+        String lastName;
+
+        if (name2.length == 2){
+             lastName = name2[1];
+        }else {
+            secondName = name2[1];
+            lastName = name2[2];
+        }
+        System.out.println("Your first name is " + firstName);
+        System.out.println("Your second name is " + secondName);
+        System.out.println("Your last name is " + lastName);
+
+    }
+
 }
